dc5623e7fe5e8fc1548aa18b5eed1fbf7326f4fd
Nils Ueter <nils.ueter@gmail.com>
2020-12-14 15:51:12 +0100

    Day 14 Part 2

--- 14/main.js
+++ 14/main.js
@@ -33,5 +33,37 @@ const part1 = () => {
 };
 console.log("Part 1: " + part1());
 
-const part2 = () => {};
+function shiftify(values) {
+  return values.map((v) => v << 1n);
+}
+
+function oneify(values) {
+  return values.map((v) => v | 1n);
+}
+
+const part2 = () => {
+  let mask;
+  const memory = {};
+  for (const line of input) {
+    const { address, value } = line;
+    if (address) {
+      const addressBinarString = address.toString(2).padStart(36, "0");
+      let addresses = [0n];
+      [...mask].forEach((element, index) => {
+        addresses = shiftify(addresses);
+        if (element === "X") {
+          addresses = [...addresses, ...oneify(addresses)];
+        } else if (element === "1" || addressBinarString[index] === "1") {
+          addresses = oneify(addresses);
+        }
+      });
+      addresses.forEach((a) => {
+        memory[a] = value;
+      });
+    } else {
+      mask = line;
+    }
+  }
+  return Object.values(memory).reduce((acc, current) => acc + current, 0n);
+};
 console.log("Part 2: " + part2());
